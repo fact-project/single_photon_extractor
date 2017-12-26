89601af472c51e671d40db97f60fe1aaf57241c8
sebastian <sebmuell@phys.ethz.ch>
2017-12-26 10:04:37 +0100

    pep8, rm unused imports

--- single_photon_extractor/extractor.py
+++ single_photon_extractor/extractor.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python
-from __future__ import absolute_import, print_function, division
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-from tqdm import tqdm
-import matplotlib.patches as mpatches
 
-#-------------------------------------------------------------------------------
+
 def time_slices(f_sample, N):
     """
     Sample moment times
@@ -18,14 +14,16 @@ def time_slices(f_sample, N):
 
     Returns
     -------
-    Returns the sample moment times (array like) of a timeline of length N 
+    Returns the sample moment times (array like) of a timeline of length N
     and with sample frequency f_sample.
     """
     return np.linspace(0, N/f_sample, num=N, endpoint=False)
-#-------------------------------------------------------------------------------
+
+
 def power_spectrum(sig_vs_t, f_sample):
     """
-    Power frequecy spectrum of a signal time line sig_vs_t of sample frequency f_sample
+    Power frequecy spectrum of a signal time line sig_vs_t of sample
+    frequency f_sample
 
     Parameters
     ----------
@@ -45,7 +43,8 @@ def power_spectrum(sig_vs_t, f_sample):
     spec = spec[idx]
     spec = np.sqrt(np.real(spec)**2.0 + np.imag(spec)**2.0)
     return freq, spec
-#-------------------------------------------------------------------------------
+
+
 def sinus_vs_t(f_sample, N, f_sinus):
     """
     Sine wave amplitude vs time
@@ -53,17 +52,19 @@ def sinus_vs_t(f_sample, N, f_sinus):
     """
     t = time_slices(f_sample, N)
     return np.sin(t*f_sinus*(2.0*np.pi))
-#-------------------------------------------------------------------------------
+
+
 def approximate_ac_coupling(sig_vs_t):
     """
-    Applys a poor man's AC coupling to the signal timeline. 
-    It approximates the signal's baseline shift but neglects the 
+    Applys a poor man's AC coupling to the signal timeline.
+    It approximates the signal's baseline shift but neglects the
     causal order of the pulses. Since there are no DC currents on long time
-    scales, it enforces the average signal over a long periode of time to be 
+    scales, it enforces the average signal over a long periode of time to be
     zero.
     """
     sig_vs_t -= np.mean(sig_vs_t)
-#-------------------------------------------------------------------------------
+
+
 def sipm_vs_t(f_sample, N, t_offset):
     """
     The FACT SIPM pulse amplitude vs time
@@ -75,15 +76,16 @@ def sipm_vs_t(f_sample, N, t_offset):
 
     # evaluate template, here the equation is for nano seconds
     s_vs_t = 1.626*(1.0-np.exp(-0.3803*t))*np.exp(-0.0649*t)
-    
-    # Since this is only a polynomial approx. 
+
+    # Since this is only a polynomial approx.
     # We truncate artefacts like negative amplitudes
     s_vs_t[s_vs_t < 0.0] = 0.0
     return s_vs_t
-#-------------------------------------------------------------------------------
+
+
 def add_first_to_second_at(f1, f2, injection_slices):
     """
-    Adds the first 1D array to the second 1D array at all injection 
+    Adds the first 1D array to the second 1D array at all injection
     slices of the third 1D array.
     """
     for injection_slice in injection_slices:
@@ -109,7 +111,8 @@ def add_first_to_second_at(f1, f2, injection_slices):
             e1 = e2-injection_slice
 
         f2[injection_slice:e2] += f1[0:e1]
-#-------------------------------------------------------------------------------
+
+
 def white_noise(N, sigma):
     """
     white gaussian noise amplitudes
@@ -117,10 +120,11 @@ def white_noise(N, sigma):
     if sigma == 0:
         return np.zeros(N)
     return np.random.normal(loc=0.0, scale=sigma, size=(N))
-#-------------------------------------------------------------------------------
+
+
 def poisson_arrival_times(exposure_time, f):
     """
-    poisson distributed arrival times of pulses in a given exposure time 
+    poisson distributed arrival times of pulses in a given exposure time
     exposure_time with an average frequency of f
     """
     arrival_times = []
@@ -132,9 +136,12 @@ def poisson_arrival_times(exposure_time, f):
             arrival_times.append(time)
 
     return np.array(arrival_times)
-#-------------------------------------------------------------------------------
+
+
 def fact_sig_vs_t(cfg):
-    arrival_times = poisson_arrival_times(cfg['T_sample']*cfg['psc_N'], cfg['f_photons'])
+    arrival_times = poisson_arrival_times(
+        cfg['T_sample']*cfg['psc_N'],
+        cfg['f_photons'])
     arrival_slices = np.floor(arrival_times*cfg['f_sample']).astype('int')
 
     mctruth = {}
@@ -144,44 +151,46 @@ def fact_sig_vs_t(cfg):
 
     add_first_to_second_at(
         sipm_vs_t(cfg['f_sample'], 300, 0.0),
-        psc_vs_t, 
+        psc_vs_t,
         mctruth['pulse_injection_slices']
     )
 
     approximate_ac_coupling(psc_vs_t)
     psc_vs_t = psc_vs_t + white_noise(cfg['psc_N'], cfg['std_dev_el_noise'])
 
-    mctruth['pulse_injection_slices'] = mctruth['pulse_injection_slices']-cfg['roi_start']
+    mctruth['pulse_injection_slices'] = (
+        mctruth['pulse_injection_slices'] -
+        cfg['roi_start'])
     return psc_vs_t[cfg['roi_start']: cfg['roi_start']+cfg['roi_N']], mctruth
 
-#-------------------------------------------------------------------------------
+
 def plot_timeline_with_mc_truth(f_sample, s_vs_t_and_mc):
     s_vs_t = s_vs_t_and_mc[0]
     mc = s_vs_t_and_mc[1]
     times = 1e9*time_slices(f_sample, s_vs_t.shape[0])
 
-    ax1 = plt.subplot(4,1,1)
+    ax1 = plt.subplot(4, 1, 1)
     ax1.step(times, s_vs_t)
     ax1.set_xlabel('t/ns')
     ax1.set_ylabel('A/1')
 
-    ax2 = plt.subplot(4,1,2, sharex=ax1)
+    ax2 = plt.subplot(4, 1, 2, sharex=ax1)
     for s in mc['pulse_injection_slices']:
         if s < 0:
             continue
-        ax2.plot([times[s], times[s]],[0, 1], 'r')
+        ax2.plot([times[s], times[s]], [0, 1], 'r')
 
     ax2.set_xlabel('t/ns')
     ax2.set_ylabel('truth')
 
-    ax3 = plt.subplot(4,1,3)
+    ax3 = plt.subplot(4, 1, 3)
     ax3.set_xlabel('t/ns')
     ax3.set_ylabel('convolve')
     sipm = sipm_vs_t(f_sample, 20, 0.0)
     fcp = np.convolve(s_vs_t, sipm, mode='same')/sum(sipm)
     ax3.plot(times, fcp-fcp.min())
 
-    ax4 = plt.subplot(4,1,4)
+    ax4 = plt.subplot(4, 1, 4)
     frec, spec = power_spectrum(s_vs_t, f_sample)
     ax4.step(frec, spec)
     ax4.set_xlabel('f/Hz')
@@ -190,8 +199,13 @@ def plot_timeline_with_mc_truth(f_sample, s_vs_t_and_mc):
 
     plt.show()
 
-#-------------------------------------------------------------------------------
-def extraction(sig_vs_t, puls_template, subs_pulse_template, return_intermediate_sig_vs_t=False):
+
+def extraction(
+    sig_vs_t,
+    puls_template,
+    subs_pulse_template,
+    return_intermediate_sig_vs_t=False
+):
     ri = return_intermediate_sig_vs_t
     intermediate_sig_vs_t = []
 
@@ -199,13 +213,13 @@ def extraction(sig_vs_t, puls_template, subs_pulse_template, return_intermediate
     arrivalSlices = []
     puls_template_integral = sum(puls_template)
 
-    if ri: 
+    if ri:
         intermediate_sig_vs_t.append(sig_vs_t_copy.copy())
 
     while True:
         sig_conv_sipm = np.convolve(
-            sig_vs_t_copy, 
-            puls_template, 
+            sig_vs_t_copy,
+            puls_template,
             mode='valid'
         )/puls_template_integral
 
@@ -215,12 +229,15 @@ def extraction(sig_vs_t, puls_template, subs_pulse_template, return_intermediate
 
         if max_response >= 0.45:
 
-            add_first_to_second_at(subs_pulse_template, sig_vs_t_copy, [max_slice])
+            add_first_to_second_at(
+                subs_pulse_template,
+                sig_vs_t_copy,
+                [max_slice])
             approximate_ac_coupling(sig_vs_t_copy)
 
             if max_slice > 0:
                 arrivalSlices.append(max_slice)
-                if ri: 
+                if ri:
                     intermediate_sig_vs_t.append(sig_vs_t_copy.copy())
         else:
             break
@@ -232,27 +249,26 @@ def extraction(sig_vs_t, puls_template, subs_pulse_template, return_intermediate
         return arrivalSlices
 
 
-#-------------------------------------------------------------------------------
-# 
 # Welcome to the FACT single photon pulse extraction sandbox
 #
 # 1) SETUP
 # --------
 cfg = {}
-cfg['f_sample'] = 2e9 
-cfg['roi_N'] = 300 #roi: region of interest
-cfg['psc_overhead'] = 3 #psc: pseudo continuos timeline
-cfg['f_photons'] = 5e7 # average arrival frequency of photons (poisson distributed)
-cfg['std_dev_el_noise'] = 0.1 # electronic noise amplitude in std dev of pulse hights 
+cfg['f_sample'] = 2e9
+cfg['roi_N'] = 300  # roi: region of interest
+cfg['psc_overhead'] = 3  # psc: pseudo continuos timeline
+cfg['f_photons'] = 5e7  # average arrival frequency of photons
 
-cfg['T_sample'] = 1.0/cfg['f_sample'] 
+# electronic noise amplitude in std dev of pulse hights
+cfg['std_dev_el_noise'] = 0.1
+cfg['T_sample'] = 1.0/cfg['f_sample']
 cfg['roi_start'] = cfg['psc_overhead']*cfg['roi_N']
 cfg['psc_N'] = (cfg['psc_overhead']+1)*cfg['roi_N']
 
 # 2) PULS TEMPLATE
 # ----------------
 # pulse template of the puls to be extracted
-# We do not use the full puls but only its rising edge and peak. 
+# We do not use the full puls but only its rising edge and peak.
 # We leave out the tail
 puls_template = sipm_vs_t(cfg['f_sample'], N=20, t_offset=0.0)
 
