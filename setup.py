b397f49c524f339f2b347808a136c11b3408b977
sebastian <sebmuell@phys.ethz.ch>
2017-12-26 09:51:19 +0100

    make it pip installable

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='single_photon_extractor',
+    version='0.0.1',
+    description='A sandbox to explore single photon extraction',
+    url='https://github.com/fact-project/',
+    author='Sebastian Achim Mueller',
+    author_email='sebmuell@phys.ethz.ch',
+    license='GPLv3',
+    packages=[
+        'single_photon_extractor',
+    ],
+    package_data={
+        'single_photon_extractor': []
+    },
+    install_requires=[
+        'docopt',
+        'scipy',
+        'matplotlib',
+        'pyfact',
+        'pandas',
+    ],
+    entry_points={'console_scripts': []},
+    zip_safe=False,
+)
